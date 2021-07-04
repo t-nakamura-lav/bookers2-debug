31cf1c85c1a9bcf35f298e013ce4efc3b5f7e606
1am1goodtime@gmail.com <1am1goodtime@gmail.com>
2021-07-04 10:15:22 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,16 +25,18 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user
+      if @user != current_user
+        redirect_to books_path
+      end
   end
-
-
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render 'edit'
     end
   end
 
