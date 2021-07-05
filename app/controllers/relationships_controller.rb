413d384a58663042bf79daf568e17c2ea8b01195
1am1goodtime@gmail.com <1am1goodtime@gmail.com>
2021-07-05 10:29:21 +0000

    follow-2

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,7 +7,7 @@ class RelationshipsController < ApplicationController
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    redirect_to request.referer
   end
 #————————フォロー・フォロワー一覧を表示する-————————————
   def followings
